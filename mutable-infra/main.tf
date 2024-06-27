ad13f2d52fca3fd474224f2a401f02d0d1b56101
Adarsh-Pixel <kumaradarsh921@gmail.com>
2024-06-27 13:34:10 +0530

    Git Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -8,5 +8,6 @@ module "user" {
     OD_INSTANCE_COUNT    = var.OD_INSTANCE_COUNT
     APP_PORT             = var.APP_PORT
     INTERNAL             = var.INTERNAL
+    APP_VERSION          = var.APP_VERSION
 
 }
